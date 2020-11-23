7cca8b4651656d76d250df1884545418d109a519
fadelkaadan <fadelkaadan@gmail.com>
2020-11-23 13:23:22 +0100

    Initial stories layout

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { fetchStoryIds, TOP_STORIES } from "../../api/hackernews";
+import Story from "../Story";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #1d1d1d;
+`;
+
+const StoriesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-content: baseline;
+  width: 100%;
+  max-width: 1200px;
+  margin-top: 25px;
+`;
+
+const Home = () => {
+  const [storyIds, setStoryIds] = useState<number[]>([]);
+
+  useEffect(() => {
+    fetchStoryIds(TOP_STORIES, 30).then((data) => setStoryIds(data));
+  }, []);
+
+  return (
+    <Wrapper>
+      <StoriesContainer>
+        {storyIds.map((id) => (
+          <Story storyId={id} />
+        ))}
+      </StoriesContainer>
+    </Wrapper>
+  );
+};
+
+export default Home;
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { fetchStory } from "../api/hackernews";
+import styled, { keyframes } from "styled-components";
+import Author from "./Author";
+import Score from "./Score";
 
 interface StoryProps {
   storyId: number;
@@ -18,6 +21,50 @@ interface IStory {
   url: string;
 }
 
+const animation = keyframes`
+  0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+`;
+
+const Wrapper = styled.div`
+  border-radius: 15px;
+  color: #e7e7e7;
+  width: 500px;
+  margin: 0px 25px 25px 25px;
+  display: flex;
+  background-color: ${(props) => props.theme.main.background};
+`;
+
+const Sideline = styled.div`
+  width: 5%;
+  height: 100%;
+  /* background: linear-gradient(-45deg, #020024, #680979, #00d4ff); */
+  /* background: linear-gradient(-45deg, #1CB5E0 0%, #000851 100%); */
+  background: linear-gradient(-45deg, #fc466b 0%, #3f5efb 100%);
+  background-size: 400% 400%;
+  animation: ${animation} 15s ease infinite;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+`;
+
+const Link = styled.a`
+  color: #e7e7e7;
+  text-decoration: none;
+`;
+
+const Title = styled.h4``;
+
+const ContentWrapper = styled.div`
+  margin: 0 10px;
+`;
+
 const Story = ({ storyId }: StoryProps) => {
   const [story, setStory] = useState<IStory>();
 
@@ -28,11 +75,16 @@ const Story = ({ storyId }: StoryProps) => {
   const renderStory = () => {
     if (story && story.url) {
       return (
-        <li>
-          <a href={story.url} target="blank">
-            <h4>Title: {story.title}</h4>
-          </a>
-        </li>
+        <Wrapper>
+          <Sideline />
+          <ContentWrapper>
+            <Link href={story.url} target="blank">
+              <Title>{story.title}</Title>
+            </Link>
+            <Score score={story.score} />
+            <Author name={story.by} />
+          </ContentWrapper>
+        </Wrapper>
       );
     }
     return null;
