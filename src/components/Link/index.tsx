7fadc6f8dbe125b029908c127f138d2479ee19aa
fadelkaadan <fadelkaadan@gmail.com>
2020-11-26 18:46:29 +0100

    Add a copy to clipboard feature

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import copy from "copy-to-clipboard";
 
 interface LinkProps {
   src: string;
 }
 
 const Wrapper = styled.div`
+  cursor: pointer;
+`;
+
+const Button = styled.button`
   font-size: 10px;
+  padding: 0;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  color: ${(props) => props.theme.main.textColor};
+
+  &:focus {
+    outline: none;
+    border: none;
+  }
+
+  &::after {
+    content: ""; /* This is necessary for the pseudo element to work. */ 
+    display: block; /* This will put the pseudo element on its own line. */
+    width: 0%; /* Change this to whatever width you want to have before hover. */
+    padding-top: 3px; /* This creates some space between the element and the border. */
+    border-bottom: 2px solid #785bf7; /* This creates the border. Replace black with whatever color you want. */
+    transition: .5s; /* This establishes the amount of time in seconds the animation should take from start to finish */
+  }
+
+  &:hover::after {
+    width: 100%; /* This will be the new width of your border when on hover */
+  }
 `;
 
 const Link = ({ src }: LinkProps) => {
@@ -15,9 +45,15 @@ const Link = ({ src }: LinkProps) => {
     return hostname;
   };
 
+  const addToClipboard = (text: string) => {
+    copy(text);
+  };
+
   return (
     <Wrapper>
-      <p>{getHostname(src)}</p>
+      <Button onClick={() => addToClipboard(src)}>
+        <FontAwesomeIcon icon={faCopy} /> {getHostname(src)}
+      </Button>
     </Wrapper>
   );
 };
