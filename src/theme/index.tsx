a9a4c549da9790b27d487c7abfb32763e157d510
fadelkaadan <fadelkaadan@gmail.com>
2020-11-29 16:27:56 +0100

    Move the theme to the theme folder & add darkTheme

---
+++ src/theme/index.tsx
@@ -0,0 +1,27 @@
+export const darkTheme = {
+  main: {
+    background: "#1d1d1d",
+    text: {
+      primary: "#e7e7e7",
+      secondary: "#cccccc",
+    },
+  },
+  cards: {
+    background: "#2b2b2b",
+    shadow: "transparent",
+  },
+};
+
+export const lightTheme = {
+  main: {
+    background: "#d1d1d1",
+    text: {
+      primary: "#1d1d1d",
+      secondary: "#2b2b2b",
+    },
+  },
+  cards: {
+    background: "#dfdfdf",
+    shadow: "#b3b3b3",
+  },
+};
