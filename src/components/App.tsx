05e79966d8e075020cfd22874b45b6a18ef04170
fadelkaadan <fadelkaadan@gmail.com>
2020-11-29 16:28:34 +0100

    Move GlobalStyle to the theme folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,13 @@
 import React from "react";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../theme/Globalstyle";
+import { darkTheme, lightTheme } from "../theme";
 import Home from "../pages/Home";
 import "fontsource-roboto";
 
-const theme = {
-  main: {
-    background: "#1d1d1d",
-    text: {
-      primary: "#e7e7e7",
-      secondary: "#cccccc"
-    }
-  },
-  cards: {
-    background: "#2b2b2b",
-  },
-};
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0px;
-    padding: 0px;
-    font-family: roboto;
-    background-color: #1d1d1d;
-  }
-`;
-
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Home />
     </ThemeProvider>
