08b9ca40aaff0f1184299ba361165c525d790f12
fadelkaadan <fadelkaadan@gmail.com>
2020-11-30 23:49:58 +0100

    Render the header and pass themeToggler to it

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../../theme/Globalstyle";
 import { darkTheme, lightTheme } from "../../theme";
+import { useDarkMode } from "../../hooks/useDarkMode";
+
 import Home from "../../pages/Home";
 import "fontsource-roboto";
+import Header from "../Header";
 
 const App = () => {
+  const [theme, themeToggler]: any = useDarkMode();
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <GlobalStyle />
+      <Header themeToggler={themeToggler} />
       <Home />
     </ThemeProvider>
   );
