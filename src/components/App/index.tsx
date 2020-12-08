4b62c9abeac83f0a94d9a41eb285448ec477ed39
fadelkaadan <fadelkaadan@gmail.com>
2020-12-08 23:24:33 +0100

    Combine customizations and pass them to the theme provider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../../theme/Globalstyle";
-import { darkTheme, lightTheme } from "../../theme";
+import { darkTheme, lightTheme, col_1, col_2 } from "../../theme";
 import { useDarkMode } from "../../hooks/useDarkMode";
+import { useLayout } from "../../hooks/useLayout";
 
 import Home from "../../pages/Home";
 import "fontsource-roboto";
@@ -10,11 +11,34 @@ import Header from "../Header";
 
 const App = () => {
   const [theme, themeToggler]: any = useDarkMode();
+  const [layout, layoutToggler]: any = useLayout();
+
+  const combineCustomizations = () => {
+    let customizations = {};
+    if (theme === "dark") {
+      customizations = { ...customizations, ...darkTheme };
+    } else if (theme === "light") {
+      customizations = { ...customizations, ...lightTheme };
+    }
+
+    if (layout === "col-1") {
+      customizations = { ...customizations, ...col_1 };
+    } else if (layout === "col-2") {
+      customizations = { ...customizations, ...col_2 };
+    }
+
+    return customizations;
+  };
 
   return (
-    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+    <ThemeProvider theme={combineCustomizations()}>
       <GlobalStyle />
-      <Header theme={theme} themeToggler={themeToggler} />
+      <Header
+        theme={theme}
+        themeToggler={themeToggler}
+        layout={layout}
+        layoutToggler={layoutToggler}
+      />
       <Home />
     </ThemeProvider>
   );
