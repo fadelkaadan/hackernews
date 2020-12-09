3e95da8b6f1439c53930915a25a8a8eb00c03fda
fadelkaadan <fadelkaadan@gmail.com>
2020-12-09 22:04:12 +0100

    Pack votes and time in TopInfo

---
+++ src/components/TopInfo/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Score from "../Score";
+import Time from "../Time";
+
+interface TopInfoProps {
+  time: number;
+  score: number;
+}
+
+const Wrapper = styled.div`
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme.main.text.secondary};
+`;
+
+const TopInfo = ({ time, score }: TopInfoProps) => {
+  return (
+    <Wrapper>
+      <Time time={time}/>
+      <Score count={score} />
+    </Wrapper>
+  );
+};
+
+export default TopInfo;
