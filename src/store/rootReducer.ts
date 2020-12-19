ca7b25984b16b81806e014f42ed68b5aca12d273
fadelkaadan <fadelkaadan@gmail.com>
2020-12-19 12:43:03 +0100

    Remove redux store. Will restore later

--- src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import theme from "./theme";
-
-const rootReducer = combineReducers({
-  theme,
-});
-
-export default rootReducer;
-export type RootState = ReturnType<typeof rootReducer>;
