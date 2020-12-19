316c532a7a209039d1b16ba806c4b6ecd18e5aa7
fadelkaadan <fadelkaadan@gmail.com>
2020-12-19 11:43:12 +0100

    Remove theming logic from App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,46 +1,19 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "../../theme/Globalstyle";
-import { darkTheme, lightTheme, col_1, col_2 } from "../../theme";
-import { useDarkMode } from "../../hooks/useDarkMode";
-import { useLayout } from "../../hooks/useLayout";
-
+import { Provider } from "react-redux";
+import ThemeProvider from "../../providers/ThemeProvider";
+import store from "../../store";
 import Home from "../../pages/Home";
 import "fontsource-roboto";
 import Header from "../Header";
 
 const App = () => {
-  const [theme, themeToggler]: any = useDarkMode();
-  const [layout, layoutToggler]: any = useLayout();
-
-  const combineCustomizations = () => {
-    let customizations = {};
-    if (theme === "dark") {
-      customizations = { ...customizations, ...darkTheme };
-    } else if (theme === "light") {
-      customizations = { ...customizations, ...lightTheme };
-    }
-
-    if (layout === "col-1") {
-      customizations = { ...customizations, ...col_1 };
-    } else if (layout === "col-2") {
-      customizations = { ...customizations, ...col_2 };
-    }
-
-    return customizations;
-  };
-
   return (
-    <ThemeProvider theme={combineCustomizations()}>
-      <GlobalStyle />
-      <Header
-        theme={theme}
-        themeToggler={themeToggler}
-        layout={layout}
-        layoutToggler={layoutToggler}
-      />
-      <Home />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <Header />
+        <Home />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
