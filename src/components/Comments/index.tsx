fd8f355605d1586bdf0665f088a93cec31acb398
fadelkaadan <fadelkaadan@gmail.com>
2020-12-31 22:31:33 +0100

    Handle click on the comments button

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,21 +1,34 @@
 import React from "react";
-
-interface IComment {
-  by: string;
-  id: number;
-  kids: number[];
-  parent: number;
-  text: string;
-  time: number;
-  type: string;
-}
+import styled from "styled-components";
+import Comment from "./Comment";
+import LoadingIndicator from "../../components/LoadingIndicator";
+import { IComment } from "../../core/types";
 
 interface CommentsProps {
   data: IComment[];
+  isPending: Boolean;
+  errorMessage: any;
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  color: white;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 2.5rem;
+  grid-column: content;
+`;
+
 const Comments = ({ data }: CommentsProps) => {
-  return <div>Comments</div>;
+  const renderComments = () =>
+    data && data.map((comment) => <Comment key={comment.id} data={comment} />);
+
+  return (
+    <Wrapper>
+      {data.length < 1 ? <LoadingIndicator /> : renderComments()}
+    </Wrapper>
+  );
 };
 
 export default Comments;
--- src/components/Stories/Story/Content/index.tsx
+++ src/components/Stories/Story/Content/index.tsx
@@ -1,27 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import TopInfo from "../TopInfo";
 import Link from "./Link";
-import Comments from "../Comments";
-
-interface IStory {
-  by: string;
-  descendants: number;
-  id: number;
-  kids: number[];
-  score: number;
-  text: string;
-  time: number;
-  title: string;
-  type: string;
-  url: string;
-}
+import CommentsButton from "../../../../common/CommentsButton";
+import CommentsContainer from "../../../../containers/CommentsContainer";
+import { IStory } from "../../../../core/types";
+import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
 
 interface ContentProps {
   data: IStory;
 }
 
 const Wrapper = styled.div`
+  width: 100%;
   padding: 0.5rem 1.5rem 0.5rem 1.5rem;
   display: flex;
   flex-direction: column;
@@ -44,6 +35,12 @@ const StyledLink = styled.div`
 `;
 
 const Content = ({ data }: ContentProps) => {
+  const [isCommentsOpen, setIsCommentsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsCommentsOpen(!isCommentsOpen);
+  };
+
   return (
     <Wrapper>
       <TopInfo time={data.time} score={data.score} />
@@ -51,7 +48,13 @@ const Content = ({ data }: ContentProps) => {
         <Title href={data.url}>{data.title}</Title>
         <Link src={data.url} />
       </StyledLink>
-      <Comments comments={data.kids} />
+      <CommentsButton
+        comments={data.kids}
+        onClick={handleClick}
+        label={data.kids && data.kids.length === 1 ? "comment" : "comments"}
+        icon={faCommentDots}
+      />
+      {isCommentsOpen ? <CommentsContainer commentIds={data.kids} /> : null}
     </Wrapper>
   );
 };
