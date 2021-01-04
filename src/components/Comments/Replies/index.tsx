88e964272ba5b1eb457ee2d67dc1aa54f0dedd70
Fadel Kaadan <fadelkaadan@gmail.com>
2021-01-04 23:51:44 +0100

    Restructuring (#21)

---
+++ src/components/Comments/Replies/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import CommentsContainer from "../../../containers/CommentsContainer";
+import CommentsButton from "../../CommentsButton";
+import { faReply } from "@fortawesome/free-solid-svg-icons";
+
+interface RepliesProps {
+  data: number[];
+}
+
+const Replies = ({ data }: RepliesProps) => {
+  const [isCommentsOpen, setIsCommentsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsCommentsOpen(!isCommentsOpen);
+  };
+
+  return (
+    <>
+      <CommentsButton
+        comments={data}
+        isOpen={isCommentsOpen}
+        onClick={handleClick}
+        label={data && data.length === 1 ? "reply" : "replies"}
+        icon={faReply}
+      />
+      {isCommentsOpen ? <CommentsContainer commentIds={data} /> : null}
+    </>
+  );
+};
+
+export default Replies;
