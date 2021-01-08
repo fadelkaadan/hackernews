0b719df06a7eae9d0e72669338ef11801e8f5178
fadelkaadan <fadelkaadan@gmail.com>
2021-01-08 23:52:50 +0100

    Add user to root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import preferences from "./preferences";
 import stories from "./stories";
+import user from "./user";
 
 const rootReducer = combineReducers({
   preferences,
   stories,
+  user,
 });
 
 export default rootReducer;
