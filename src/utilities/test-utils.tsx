e7c61ea7e3a3fb6ac1efefa807a2966521b46309
fadelkaadan <fadelkaadan@gmail.com>
2021-01-17 16:56:30 +0100

    Setup test utils

---
+++ src/utilities/test-utils.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Provider } from "react-redux";
+import ThemeProvider from "../theme/ThemeProvider";
+import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { ThemeEnum } from "../store/preferences/types";
+import { RootState } from "../store/rootReducer";
+
+interface AllTheProvidersProps {
+  children?: any;
+}
+
+const AllTheProviders = ({ children }: AllTheProvidersProps) => {
+  const middlewares: any[] = [];
+  const mockStore = configureStore(middlewares);
+
+  const intialState: RootState = {
+    preferences: {
+      isPreferencesClicked: false,
+      theme: ThemeEnum.NIGHT,
+      isOneColumnLayout: true,
+    },
+    stories: {
+      list: [],
+      startAt: 0,
+      pending: false,
+      error: null,
+    },
+    user: {
+      info: null,
+      pending: false,
+      error: null,
+
+      stories: {
+        list: [],
+        startAt: 0,
+        pending: false,
+        error: null,
+      },
+    },
+  };
+  const store = mockStore(intialState);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider>{children}</ThemeProvider>
+      </Router>
+    </Provider>
+  );
+};
+
+const customRender = (ui: any, options?: any) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
