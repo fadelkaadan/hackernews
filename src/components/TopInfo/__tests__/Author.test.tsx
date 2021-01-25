14ed4cef34c08fbeeed9585924497f2625c953d8
fadelkaadan <fadelkaadan@gmail.com>
2021-01-25 21:31:25 +0100

    Test author

---
+++ src/components/TopInfo/__tests__/Author.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen, RenderResult } from "../../../utilities/test-utils";
+import Author from "../Author";
+
+describe("Comment", () => {
+  const name = "fadel";
+
+  it("Should match the snapshot", () => {
+    const { container }: RenderResult = render(<Author name={name} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("Should render author name", () => {
+    render(<Author name={name} />);
+    const author: HTMLElement = screen.getByRole("link", {name: name});
+    expect(author).toBeInTheDocument();
+  });
+
+  it("Should render author name", () => {
+    const { container }: RenderResult = render(<Author name={name} />);
+    const userIcon = container.querySelector("svg");
+    expect(userIcon).toBeDefined();
+  });
+
+});
